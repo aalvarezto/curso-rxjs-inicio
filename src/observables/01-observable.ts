232f4817bc04ae95ef0a352cea3c03f15e3299c3
Andres Alvarez <ndralvz@gmail.com>
2021-01-17 23:55:03 -0300

    fist commit

---
+++ src/observables/01-observable.ts
@@ -0,0 +1,30 @@
+"use strict"
+
+import { Observable, Observer } from "rxjs"
+
+const observer: Observer<any> = {
+	next: value => console.log("siguiente [next]:", value),
+	error: error => console.warn("error [obs]:", error),
+	complete: () => console.info("completado [obs]"),
+}
+
+const obs$ = new Observable<string>(subs => {
+	subs.next("Hola")
+	subs.next("Mundo")
+
+	subs.next("Hola")
+	subs.next("Mundo")
+
+	// const a = undefined
+	// a = a.map(v => v)
+
+	subs.complete()
+})
+
+obs$.subscribe(observer)
+
+// obs$.subscribe(
+// 	valor => console.log("next: ", valor),
+// 	error => console.warn("error: ", error),
+// 	() => console.log("Completado")
+// )
